69091de8564923e114ff406ec8e6cfbc784cc7cb
JYunth <jyunth28@gmail.com>
2024-01-21 00:42:48 +0530

    Add basic timer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 // App.js
 import React from "react";
 import Countdown from "./Countdown";
+import Countdown2 from "./Countdown2";
 
 const App = () => {
   return (
-    <div>
-      <Countdown />
-    </div>
+    <>
+      <div>
+      <Countdown2 />
+      </div>
+    </>
   );
 };
 
